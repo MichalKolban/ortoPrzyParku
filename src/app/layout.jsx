7a7d64bf5ce0cb85ec7876727622fe572e41c176
michal kolban <michal.kolban@gmail.com>
2025-01-08 12:01:17 +0100

    ortoPrzyParku ver 0.0.7

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,48 +1,31 @@
-'use client';
+"use client";
 
-import localFont from 'next/font/local';
-import './globals.css';
-import { useState, useEffect } from 'react';
+import "./globals.css";
 
-import { Footer } from './components/Footer/Footer';
-import { Navigation } from './components/Navigation/Navigation';
-import { Montserrat } from 'next/font/google';
-import { MySign } from './components/MySign/MySign';
+import Head from "next/head";
+
+import { Footer } from "./components/Footer/Footer";
+import { Navigation } from "./components/Navigation/Navigation";
+import { Montserrat } from "next/font/google";
+import { MySign } from "./components/MySign/MySign";
 
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600'],
-  variable: '--font-montserrat',
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600"],
+  variable: "--font-montserrat",
 });
 
 export default function RootLayout({ children }) {
-  // const checkWindowSize = () => {
-  //   if (typeof window !== 'undefined') {
-  //     const windowWidth = window.innerWidth;
-  //     if (windowWidth >= 1024) {
-  //       setDeviceType('desktop');
-  //     } else if (windowWidth >= 768 && windowWidth < 1024) {
-  //       setDeviceType('tablet');
-  //     } else {
-  //       setDeviceType('mobile');
-  //     }
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   checkWindowSize(); // Pierwsze sprawdzenie po załadowaniu komponentu
-  //   // Nasłuchuj zmian rozmiaru okna
-  //   window.addEventListener('resize', checkWindowSize);
-  //   return () => {
-  //     window.removeEventListener('resize', checkWindowSize); // Usuń nasłuchiwacz przy unmount
-  //   };
-  // }, []);
-
   return (
     <html lang="pl">
+      <Head>
+        <title>My App</title>
+        <meta name="description" content="This is my amazing app!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body className={montserrat.variable}>
         <Navigation />
-        <main className={'mainWrapper'}>{children}</main>
+        <main className={"mainWrapper"}>{children}</main>
         <Footer />
         <MySign />
       </body>
