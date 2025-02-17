6d9d434a5de89e444ad5ec500b398f6e4b987fe7
michal kolban <michal.kolban@gmail.com>
2025-02-17 20:43:00 +0100

    ortoPrzyParku ver 0.1.77

--- src/app/szkolenia/page.jsx
+++ src/app/szkolenia/page.jsx
@@ -34,28 +34,30 @@ const SzkoleniaPage = () => {
             <h2 className={style.mainTitle}>
               Lekarz Stomatolog, Specjalista ortodona
             </h2>
-            <div className={style.imageWrapper}>
-              <Image src={szkolenieImg} width={320} height={460} />
-            </div>
-            <div className={style.textContent}>
-              Nazywam się Katarzyna Zaleska, jestem absolwentką Wydziału
-              Lekarsko- Stomatologicznego PUM w Szczecinie. Od 2001 roku, jako
-              jedna z pierwszych ortodontek w Polsce zastosowałam mikroimplant
-              do leczenia ortodontycznego. Od tego czasu rozwijam techniki
-              leczenia z wykorzystaniem mikroimplantów. Ta dziedzina stała się
-              tematem mojej pracy doktorskiej z 2009 roku, w której zbadałam
-              skuteczność zakotwienia Mikrośrubą i InPlantem w leczeniu
-              ortodontycznym. Przez ponad 20 lat zgromadziłam bogate
-              doświadczenie kliniczne w zakresie mikroimplantów, co pozwoliło mi
-              opublikować liczne prace naukowe i dzielić się wiedzą podczas
-              wykładów i szkoleń. Szczególną satysfakcję czerpię z prowadzenia
-              warsztatów, gdzie uczę innych ortodontów technik montażu i
-              wykorzystania mikroimplantów w szczęce i żuchwie w różnych wadach
-              zgryzu. Od 2020 roku prowadzę klinikę Ortodoncja Przy Parku, gdzie
-              łączę swoje doświadczenie akademickie (jako były asystent i
-              wykładowca PUM) z pasją do innowacyjnych cyfrowych technik
-              leczenia. Jestem członkiem prestiżowych towarzystw
-              ortodontycznych: PTO, EOS, PAA i FACE Poland.
+            <div className={style.doctorBox}>
+              <div className={style.imageWrapper}>
+                <Image src={szkolenieImg} width={320} height={460} />
+              </div>
+              <div className={style.textContent}>
+                Nazywam się Katarzyna Zaleska, jestem absolwentką Wydziału
+                Lekarsko- Stomatologicznego PUM w Szczecinie. Od 2001 roku, jako
+                jedna z pierwszych ortodontek w Polsce zastosowałam mikroimplant
+                do leczenia ortodontycznego. Od tego czasu rozwijam techniki
+                leczenia z wykorzystaniem mikroimplantów. Ta dziedzina stała się
+                tematem mojej pracy doktorskiej z 2009 roku, w której zbadałam
+                skuteczność zakotwienia Mikrośrubą i InPlantem w leczeniu
+                ortodontycznym. Przez ponad 20 lat zgromadziłam bogate
+                doświadczenie kliniczne w zakresie mikroimplantów, co pozwoliło
+                mi opublikować liczne prace naukowe i dzielić się wiedzą podczas
+                wykładów i szkoleń. Szczególną satysfakcję czerpię z prowadzenia
+                warsztatów, gdzie uczę innych ortodontów technik montażu i
+                wykorzystania mikroimplantów w szczęce i żuchwie w różnych
+                wadach zgryzu. Od 2020 roku prowadzę klinikę Ortodoncja Przy
+                Parku, gdzie łączę swoje doświadczenie akademickie (jako były
+                asystent i wykładowca PUM) z pasją do innowacyjnych cyfrowych
+                technik leczenia. Jestem członkiem prestiżowych towarzystw
+                ortodontycznych: PTO, EOS, PAA i FACE Poland.
+              </div>
             </div>
           </div>
         </div>
