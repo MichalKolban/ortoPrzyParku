c134a6c55d76bc92e5a62cfff37d3a43384d8c5f
michal kolban <michal.kolban@gmail.com>
2025-02-18 12:55:25 +0100

    ortoPrzyParku ver 0.1.82

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -34,6 +34,7 @@ export default function RootLayout({ children }) {
           content="Ortodoncja Przy Parku w Szczecinie – nowoczesna klinika ortodontyczna dla dzieci i dorosłych. Profesjonalne leczenie wad zgryzu, aparaty stałe i ruchome. Umów się na konsultację!"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="canonical" href="https://ortodoncjaprzyparku.pl" />
       </head>
       <body className={montserrat.variable}>
         <Navigation />
