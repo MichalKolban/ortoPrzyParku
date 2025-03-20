f363bc5e026c9753be86a1979ab2e09706aed82c
michal kolban <michal.kolban@gmail.com>
2025-03-20 19:48:15 +0100

    ortoPrzyParku ver 0.2.17 attemting to fix layout

---
+++ src/app/(szkoleniadwa)/layout.jsx
@@ -0,0 +1,8 @@
+export default function NowenoweLayout({ children }) {
+  console.log("🆕 Renderuje się NowenoweLayout!");
+  return (
+    <html lang="pl">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/(szkoleniadwa)/szkoleniadwa/page.jsx
@@ -0,0 +1,8 @@
+export default function SzkoleniaDwaPage() {
+  return (
+    <div>
+      <h1>Strona Nowenowe</h1>
+      <p>Treść strony nowenowe.</p>
+    </div>
+  );
+}
